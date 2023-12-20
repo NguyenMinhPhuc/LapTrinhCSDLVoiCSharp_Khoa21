08ce5438c01edc1fa09da7999a55766f4a3ec2e2
Nguyen Minh Phuc <nmphuc@outlook.com>
2023-12-20 16:29:01 +0700

    buoi hocj 231220

---
+++ Lessons/Lesson03_21CT113/QuanLyBanHang/QuanLyBanHang.WinForm/HeThong/FrmQuanLyNguoiDung_Main.cs
@@ -0,0 +1,37 @@
+﻿using QuanLyBanHang.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang.WinForm.HeThong
+{
+    public partial class FrmQuanLyNguoiDung_Main : Form
+    {
+        public FrmQuanLyNguoiDung_Main()
+        {
+            InitializeComponent();
+        }
+        BLL_HeThong bll;
+        string err = string.Empty;
+
+        private void FrmQuanLyNguoiDung_Main_Load(object sender, EventArgs e)
+        {
+            bll = new BLL_HeThong(ClsMain.pathConnectFile);
+            HienThiDanhSach();
+        }
+
+        private void HienThiDanhSach()
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable = bll.GetUserByID(ref err, 4.ToString());
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
